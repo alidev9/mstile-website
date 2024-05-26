686c8c47592fc03cb517a6a776b21011b1389699
ali <159271119+alidev9@users.noreply.github.com>
2024-05-27 01:11:27 +0300

    Started work on tablet view

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,6 @@ function Home(){
     return (
             <main className='homepage-container'>
                 <div className="feature-container">
-                    <img src="/mstile-website/homepage-images/float-image-1.jpg" alt=""/>
                     <div className="feature-text-wrapper">
                         <h2>
                             Award-Winning Design expertise...
@@ -17,9 +16,9 @@ function Home(){
                                 With over a decade of experience in our sector, our team of industry leading professionals can help you design a truly bespoke bathroom, that incorporates the highest standards of functionality, and meets all your individual needs and preferences.
                             </p>
                     </div>
+                    <img src="/mstile-website/homepage-images/float-image-1.jpg" alt=""/>
                 </div>
                 <div className="feature-container">
-                    <img src="/mstile-website/homepage-images/float-image-2.jpg" alt=""/>
                     <div className="feature-text-wrapper">
                         <h2>
                             Attention to detail...
@@ -31,6 +30,7 @@ function Home(){
                             We will work with you to create a unique and customized look that reflects your personal style and taste, whilst also incorporating the use of the best materials and design processes available.  
                         </p>
                     </div>
+                    <img src="/mstile-website/homepage-images/float-image-2.jpg" alt=""/>
                 </div>
                 <div className="carousel-container image-block middle-image">
                     <div className="carousel">
@@ -45,13 +45,17 @@ function Home(){
                     <p>
                         Alongside designing and fitting bathrooms, mStile also provides an array of professional tiling options, LVT and Vinyl fitting as well as our 3D illuminated  ceiling confgurations for customers who would like to add something totally different to their home.
                     </p>
-                    <img src="/mstile-website/homepage-images/float-image-3.jpg" alt="" className="floating-image-1"/>
-                    <p>
-                        3D illuminated ceilings, also known as Stretch Ceilings are a smart way to give your bathroom unqiue flare and maxermise the 'wow' effect. Made from highly durable PVC streach material  which gives increased acoustic effect, and is easily washable making an excelelnt addtion to a customised bathroom.
-                    </p>
-                    <p>
-                        If you'd like to find out more about the product in detail please <a href="#">click here</a>.
-                    </p>
+                    <div className="floating-section-wrapper">
+                        <img src="/mstile-website/homepage-images/float-image-3.jpg" alt="" className="floating-image-1"/>
+                        <div className="floating-text-wrapper">
+                            <p>
+                                3D illuminated ceilings, also known as Stretch Ceilings are a smart way to give your bathroom unqiue flare and maxermise the 'wow' effect. Made from highly durable PVC streach material  which gives increased acoustic effect, and is easily washable making an excelelnt addtion to a customised bathroom.
+                            </p>
+                            <p>
+                                If you'd like to find out more about the product in detail please <a href="#">click here</a>.
+                            </p>
+                        </div>
+                    </div>
                     <img src="/mstile-website/homepage-images/acoustic-wall-covering-designer-company 1.jpg" alt="" className="main-banner-1"/>
                     <p>
                         Streach seilings can be applied in all types of rooms in your home as far as your imagination can stretch, from bathrooms, to bedrooms, living rooms, conference rooms, kitchens, garages even a commerical setting; ideal for offices, pub bathrooms, hairdressers, restaurants, spa centres and many more.
