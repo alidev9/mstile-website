6cd5f7585d7502ceba6ddeba888194d631b2457e
Ali <159271119+alidev9@users.noreply.github.com>
2024-05-27 14:36:18 +0300

    The footer now scales down to fit tablet screens

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,29 +9,31 @@ function Footer(){
                     <a href="#"><img src="./icons/instagram-icon.svg" alt=""/></a>
                 </div>
             </div>
-            <div class="footer-column">
-                <h3>Home</h3>
-                <ul>
-                    <li><a href="#">Full Bathrooms</a></li>
-                    <li><a href="#">About Us</a></li>
-                    <li><a href="#">FAQ</a></li>
-                </ul>
-            </div>
-            <div class="footer-column">
-                <h3>Contact Us</h3>
-                <ul>
-                    <li><a href="#">Facebook</a></li>
-                    <li><a href="#">Instagram</a></li>
-                    <li><a href="#">Email</a></li>
-                </ul>
-            </div>
-            <div class="footer-column">
-                <h3>Services</h3>
-                <ul>
-                    <li><a href="#">Tiling</a></li>
-                    <li><a href="#">LVT & Vinyl</a></li>
-                    <li><a href="#">3D Projects</a></li>
-                </ul>
+            <div className="footer-text-column-wrapper">
+                <div class="footer-column">
+                    <h3>Home</h3>
+                    <ul>
+                        <li><a href="#">Full Bathrooms</a></li>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">FAQ</a></li>
+                    </ul>
+                </div>
+                <div class="footer-column">
+                    <h3>Contact Us</h3>
+                    <ul>
+                        <li><a href="#">Facebook</a></li>
+                        <li><a href="#">Instagram</a></li>
+                        <li><a href="#">Email</a></li>
+                    </ul>
+                </div>
+                <div class="footer-column">
+                    <h3>Services</h3>
+                    <ul>
+                        <li><a href="#">Tiling</a></li>
+                        <li><a href="#">LVT & Vinyl</a></li>
+                        <li><a href="#">3D Projects</a></li>
+                    </ul>
+                </div>
             </div>
         </footer>
     )
