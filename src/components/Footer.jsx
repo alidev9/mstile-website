d2898a892b28b863e5ee48461b905c1da4e6fb4c
ali <159271119+alidev9@users.noreply.github.com>
2024-05-31 21:10:46 +0300

    Finished working on the header element for mobile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,39 +1,37 @@
 function Footer(){
     return (
         <footer>
-            <div class="footer-column company-name">
+            <div class="footer-company-info">
                 <h3>mStile <br/> Design & Services</h3>
-                <div class="socials-row">
+                <div class="socials-section">
                     <a href="#"><img src="./icons/facebook-icon.svg" alt=""/></a>
                     <a href="#"><img src="./icons/instagram-icon.svg" alt=""/></a>
                     <a href="#"><img src="./icons/instagram-icon.svg" alt=""/></a>
                 </div>
             </div>
-            <div className="footer-text-column-wrapper">
-                <div class="footer-column">
-                    <h3>Home</h3>
-                    <ul>
-                        <li><a href="#">Full Bathrooms</a></li>
-                        <li><a href="#">About Us</a></li>
-                        <li><a href="#">FAQ</a></li>
-                    </ul>
-                </div>
-                <div class="footer-column">
-                    <h3>Contact Us</h3>
-                    <ul>
-                        <li><a href="#">Facebook</a></li>
-                        <li><a href="#">Instagram</a></li>
-                        <li><a href="#">Email</a></li>
-                    </ul>
-                </div>
-                <div class="footer-column">
-                    <h3>Services</h3>
-                    <ul>
-                        <li><a href="#">Tiling</a></li>
-                        <li><a href="#">LVT & Vinyl</a></li>
-                        <li><a href="#">3D Projects</a></li>
-                    </ul>
-                </div>
+            <div class="footer-text-section">
+                <h3>Home</h3>
+                <ul>
+                    <li><a href="#">Full Bathrooms</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">FAQ</a></li>
+                </ul>
+            </div>
+            <div class="footer-text-section">
+                <h3>Contact Us</h3>
+                <ul>
+                    <li><a href="#">Facebook</a></li>
+                    <li><a href="#">Instagram</a></li>
+                    <li><a href="#">Email</a></li>
+                </ul>
+            </div>
+            <div class="footer-text-section">
+                <h3>Services</h3>
+                <ul>
+                    <li><a href="#">Tiling</a></li>
+                    <li><a href="#">LVT & Vinyl</a></li>
+                    <li><a href="#">3D Projects</a></li>
+                </ul>
             </div>
         </footer>
     )
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './styles/base-styles.css'
+import './mobile-first-styles/base-styles.css'
 import App from './App.jsx'
 
 import { HashRouter } from 'react-router-dom'
